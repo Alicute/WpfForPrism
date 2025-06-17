e1c99d88117131e68d00e79a01c302032447b06d
chengliang <chengliang@lkyl.com>
2025-06-17 16:32:12 +0800

    订阅事件的学习

--- WpfAForPrism/Views/MainWindow.xaml.cs
+++ WpfAForPrism/Views/MainWindow.xaml.cs
@@ -11,17 +11,42 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Prism.Events;
+
 
 namespace WpfAForPrism.Views
 {
+
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+
+            private  readonly IEventAggregator EventAggregator;
+        public MainWindow(IEventAggregator eventAggregator)
         {
             InitializeComponent();
+            EventAggregator = eventAggregator;
+
+        }
+
+        private void BtnPubClick(object sender,RoutedEventArgs e){
+
+            EventAggregator.GetEvent<MsgEvent>().Publish("出去玩！");
+        }   
+
+        private void BtnSubClick(object sender,RoutedEventArgs e){
+
+            EventAggregator.GetEvent<MsgEvent>().Subscribe(Sub);
+        }
+        private void Sub(string obj){
+            MessageBox.Show($"订阅事件：{obj}");
+        }
+
+        private void BtnUnSubClick(object sender,RoutedEventArgs e){
+
+            EventAggregator.GetEvent<MsgEvent>().Unsubscribe(Sub);
         }
     }
 }
